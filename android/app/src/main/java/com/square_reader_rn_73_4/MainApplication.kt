7f5344c6905a3bdd94754e873752750b4bbe1563
ulrichsinn <uli@addtothenoise.com>
2024-02-22 17:14:02 -0800

    „react-native-square-reader-sdk“ added for android

--- android/app/src/main/java/com/square_reader_rn_73_4/MainApplication.kt
+++ android/app/src/main/java/com/square_reader_rn_73_4/MainApplication.kt
@@ -11,6 +11,7 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
+import com.squareup.sdk.reader.ReaderSdk;
 
 class MainApplication : Application(), ReactApplication {
 
@@ -35,6 +36,8 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
+    ReaderSdk.initialize(this)
+
     SoLoader.init(this, false)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
